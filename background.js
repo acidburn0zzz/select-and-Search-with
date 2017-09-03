d609e7a5c585702e1d4a407cbaaa2b8a733ab4a6
Olivier de Broqueville <olivier.debroqueville@gmail.com>
2017-09-03 20:06:13 +0200

    added favicons to contextMenu for firefox 56+ and did some code cleanup

--- background.js
+++ background.js
@@ -1,9 +1,36 @@
 var searchEngines = {};
 var searchEnginesArray = [];
 var selection = "";
+var browserVersion = 0;
 
 function onError(error) {
     console.log(`Error: ${error}`)
+}
+
+function gotBrowserInfo(info){
+    let v = info.version;
+    browserVersion = parseInt(v.slice(0, v.search(".") - 1));
+}
+
+function buildContextMenu(searchEngine, strId, strTitle, faviconUrl){
+    if (searchEngine.show) {
+        if (browserVersion > 55){
+            browser.contextMenus.create({
+                id: strId,
+                title: strTitle,
+                contexts: ["selection"],
+                icons: {
+                    18: faviconUrl
+                }
+            });
+        } else {
+            browser.contextMenus.create({
+                id: strId,
+                title: strTitle,
+                contexts: ["selection"]
+            });
+        }
+    }
 }
 
 // Create the context menu using the search engines listed above
@@ -17,14 +44,10 @@ function updateContextMenu(changes, area) {
             for (var se in searchEngines) {
                 var strId = index.toString();
                 var strTitle = searchEngines[se].name;
+                var url = searchEngines[se].url;
+                var faviconUrl = "https://s2.googleusercontent.com/s2/favicons?domain_url=" + url;
                 searchEnginesArray.push(se);
-                if (searchEngines[se].show) {
-                    browser.contextMenus.create({
-                        id: strId,
-                        title: strTitle,
-                        contexts: ["selection"]
-                    });
-                }
+                buildContextMenu(searchEngines[se], strId, strTitle, faviconUrl);
                 index += 1;
             }
         }
@@ -32,7 +55,7 @@ function updateContextMenu(changes, area) {
 }
 
 // Perform search based on selected search engine, i.e. selected context menu item
-browser.contextMenus.onClicked.addListener(function(info, tab) {
+function processSearch(info, tab){
     var targetUrl = "";
     var id = parseInt(info.menuItemId);
     
@@ -45,12 +68,14 @@ browser.contextMenus.onClicked.addListener(function(info, tab) {
     browser.tabs.create({
         url: targetUrl
     });
-});
+}
 
 function getSelectedText(selectedText) {
     selection = selectedText;
 }
 
-updateContextMenu();
+browser.runtime.getBrowserInfo().then(gotBrowserInfo);
 browser.storage.onChanged.addListener(updateContextMenu);
+browser.contextMenus.onClicked.addListener(processSearch);
 browser.runtime.onMessage.addListener(getSelectedText);
+updateContextMenu();
